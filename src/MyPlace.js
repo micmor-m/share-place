cc698d9f5203d88791a21206ff423360145d64fe
mic <micmor.m@gmail.com>
2020-12-26 22:02:41 -0500

    add MyPlace.js to render a new tab in the browser displaying the map with location and address shared by the user

---
+++ src/MyPlace.js
@@ -0,0 +1,19 @@
+import { Map } from "./UI/Map";
+
+class LoadedPlace {
+  constructor(coordinates, address) {
+    new Map(coordinates);
+    const headerTitleEl = document.querySelector("header h1");
+    headerTitleEl.textContent = address;
+  }
+}
+
+//to confert the current URL into an object with key/value pair to be easy to extract the information needed
+const url = new URL(location.href);
+const queryParams = url.serchParams;
+const coords = {
+  lat: +queryParams.get("lat"),
+  lng: +queryParams.get("lng"),
+};
+const address = queryParams.get("address"),
+new LoadedPlace(coords, address);
